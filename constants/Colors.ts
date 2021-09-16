9f864b59186d7044ec5d276363334a64b277a7dd
Tal Gold <tal@a-i.com>
2021-09-16 12:48:09 +0300

    Add: color pallete

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,35 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+  },
+  green: {
+    text: "#fff",
+    background: "#afc3a8",
+  },
+  yellow: {
+    text: "#fff",
+    background: "#f7d78c",
+  },
+  blue: {
+    text: "#fff",
+    background: "#a6d3f2",
+  },
+  black: {
+    text: "#fff",
+    background: "#000",
   },
 };
